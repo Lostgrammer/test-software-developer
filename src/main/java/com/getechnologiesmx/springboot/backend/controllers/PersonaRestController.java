4251b33a35fba3e6cca2eea35c1dcd31fbed186a
Lostgrammer <carlos_cmvp@hotmail.com>
2023-06-24 15:59:22 -0500

    integrar puerto de angular a spring

--- src/main/java/com/getechnologiesmx/springboot/backend/controllers/PersonaRestController.java
+++ src/main/java/com/getechnologiesmx/springboot/backend/controllers/PersonaRestController.java
@@ -3,24 +3,24 @@ package com.getechnologiesmx.springboot.backend.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
-
 import com.getechnologiesmx.springboot.backend.models.entity.Persona;
 import com.getechnologiesmx.springboot.backend.models.servicedao.IPersonaRepository;
 
-
+@CrossOrigin(origins = {"http://localhost:4200"})	
 @RestController
 @RequestMapping("/api")
 public class PersonaRestController {
 	
 	@Autowired
-	private IPersonaRepository personaRepository;//importamos la interfaz
+	private IPersonaRepository personaRepository;
 	
-	@GetMapping("/clientes")
+	@GetMapping("/personas")
 	public List<Persona> index(){
 		return personaRepository.findAll();
 	}
