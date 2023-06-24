969da4dbb6489e916fb012ff45bd1db6ca1fba27
Lostgrammer <carlos_cmvp@hotmail.com>
2023-06-24 08:56:16 -0500

    correccion entidad persona

--- src/main/java/com/getechnologiesmx/springboot/backend/models/entity/Persona.java
+++ src/main/java/com/getechnologiesmx/springboot/backend/models/entity/Persona.java
@@ -8,64 +8,63 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-public class Persona {
-	@Entity
-	@Table(name = "personas")
-	public class Directorio implements Serializable {
+@Entity
+@Table(name = "personas")
+public class Persona implements Serializable{
 
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-		private String nombre;
-		private String apellidoPaterno;
-		private String apellidoMaterno;
-		private String identificacion;
+	private String nombre;
+	private String apellidoPaterno;
+	private String apellidoMaterno;
+	private String identificacion;
 
-		public Long getId() {
-			return id;
-		}
-
-		public void setId(Long id) {
-			this.id = id;
-		}
-
-		public String getNombre() {
-			return nombre;
-		}
-
-		public void setNombre(String nombre) {
-			this.nombre = nombre;
-		}
-
-		public String getApellidoPaterno() {
-			return apellidoPaterno;
-		}
-
-		public void setApellidoPaterno(String apellidoPaterno) {
-			this.apellidoPaterno = apellidoPaterno;
-		}
-
-		public String getApellidoMaterno() {
-			return apellidoMaterno;
-		}
-
-		public void setApellidoMaterno(String apellidoMaterno) {
-			this.apellidoMaterno = apellidoMaterno;
-		}
-
-		public String getIdentificacion() {
-			return identificacion;
-		}
-
-		public void setIdentificacion(String identificacion) {
-			this.identificacion = identificacion;
-		}
-
-		public static long getSerialversionuid() {
-			return serialVersionUID;
-		}
-
-		private static final long serialVersionUID = 1L;
+	public Long getId() {
+		return id;
 	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidoPaterno() {
+		return apellidoPaterno;
+	}
+
+	public void setApellidoPaterno(String apellidoPaterno) {
+		this.apellidoPaterno = apellidoPaterno;
+	}
+
+	public String getApellidoMaterno() {
+		return apellidoMaterno;
+	}
+
+	public void setApellidoMaterno(String apellidoMaterno) {
+		this.apellidoMaterno = apellidoMaterno;
+	}
+
+	public String getIdentificacion() {
+		return identificacion;
+	}
+
+	public void setIdentificacion(String identificacion) {
+		this.identificacion = identificacion;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	private static final long serialVersionUID = 1L;
+
 }
